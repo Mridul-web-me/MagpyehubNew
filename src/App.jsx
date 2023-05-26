6bdf260857e7aa1c9c2fd48928c5d6cee31b0841
mridul islam <md.mridul.23@gmail.com>
2023-05-26 21:15:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Banner from './Components/Header/Banner/Banner'
+import Header from './Components/Header/Header'
+
+import Nav from './Components/Header/Nav/Nav'
+
+function App() {
+  return (
+    <>
+   <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,24 @@
+
+import Nav from './Nav/Nav'
+import './Header.css'
+import HeaderTop from './HeaderTop/HeaderTop'
+
+const Header = () => {
+
+    // console.log(props.cart);
+    return (
+        <div className="menu sticky-top">
+            <HeaderTop></HeaderTop>
+            <Nav></Nav>
+        </div>
+    )
+}
+
+export default Header
+
+
+
+
+
+
+
